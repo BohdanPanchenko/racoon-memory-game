1ed4514f15534f253f68a9bcf4bc7e909733b75a
Bohdan Panchenko <77073241+BohdanPanchenko@users.noreply.github.com>
2022-08-30 09:49:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -105,14 +105,14 @@ function renderCards() {
         <div class="back"><img src="images/back.png" alt="racoon image">
         <div class="front"><img alt="racoon image"></div>
     </div>`;
-            
-            card.querySelector('.front img').onload = function (){
+         card.querySelector('.card').setAttribute('data-number', `${imagePosition[i]}`);
+        card.isFlipped = false;
+        card.querySelector('.card').isStatic = false;
+        card.querySelector('.front img').onload = function (){
                 
               card.style.animationDuration = `${getAnimationDuration()}s`;
         card.style.animationName = `${animations[getRandomIntInclusive(0, animations.length - 1 )]}`;
-        card.querySelector('.card').setAttribute('data-number', `${imagePosition[i]}`);
-        card.isFlipped = false;
-        card.querySelector('.card').isStatic = false;
+       
            
         container.appendChild(card); 
             }
